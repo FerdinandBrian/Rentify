53432fb937bb4fc92641699073bdd3c6cd1ba8f3
krytop113 <javierxiang113@gmail.com>
2026-05-04 20:07:18 +0700

    penyesuaian admin + pembagian part

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
-})->name('home');
+})->name('dashboard');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
